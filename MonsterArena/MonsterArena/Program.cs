d8d7ee7148d6540370c45a4fc141206e7d0dae40
Samuel Paquette <samuelpaquet@hotmail.com>
2018-08-31 13:41:59 -0400

    Static random for base npcs

--- MonsterArena/MonsterArena/Program.cs
+++ MonsterArena/MonsterArena/Program.cs
@@ -13,7 +13,8 @@ namespace MonsterArena
                 new Goblin("Alice"),
                 new Orc("Bob"),
                 new Goblin("Charlie"),
-                new Orc("David")
+                new Orc("David"),
+                new PereFwetar("Samuel")
             };
 
             foreach (var monster in monsters)
