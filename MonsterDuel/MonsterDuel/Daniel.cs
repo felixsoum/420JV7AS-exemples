63e55ae966559db27478904c7adc59ed66c56e8e
Unknown <davide018@hotmail.com>
2018-09-06 13:03:32 -0400

    C'est lui qu'il faut prendre hahaha

--- MonsterDuel/MonsterDuel/Daniel.cs
+++ MonsterDuel/MonsterDuel/Daniel.cs
@@ -1,17 +1,97 @@
-﻿using System;
+using System.Collections.Generic;
+using MonsterArena;
 
-namespace MonsterDuel
+namespace MonsterArena
 {
-    class Goblin : Monster
+    
+    class Daniel : Monster
     {
-        public Goblin(string firstName) : base(firstName)
+        int instance = 0;
+        
+        public Daniel(string name) : base(name)
         {
-            bonusDamage = 10;
+            AddBonusStrength(75);
+            AddBonusVitality(25);
+            AddBonusDexterity(0);
+            AddBonusLuck(0);
+        
+        
+        
+          
+           
         }
 
-        protected override void OnDeath()
+
+        public override string ToString()
         {
-            Console.WriteLine($"{name}: My kin will... avenge me...");
+            if (GetData().hp < (GetData().hp/2))
+            {
+            
+                return "lowkey Daniel";
+
+            }
+            else
+            {
+                return "David Daniel";
+            }
         }
+        
+
+
+        public override int GetAttackIndex(List<MonsterData> monsters)
+        {
+            List<int> monsterWeight = new List<int>();
+
+            int Damage;
+            int instance = 0;
+            Damage = 50;
+            Damage = GetDamage();
+           
+           
+            for (int i = 0; i < monsters.Count; i++)
+            {
+                int weight = 0;
+               
+                
+
+                if (monsters[i].name == GetData().name)
+                        weight -= 10000;
+                if (monsters[i].hp <= 0)
+                    weight -= 10000;
+                
+                if (instance == 0)
+                {
+                    if (monsters[i].hp <= Damage)
+                    {
+                        weight += (monsters[i].level) * 200;
+                        weight += (monsters[i].hp) * 100;
+                    }
+
+                }
+                
+
+                weight += (monsters[i].level - 1) * 20;
+                monsterWeight.Add(weight);
+                instance++;
+            }
+
+
+
+            
+
+
+            int index = 0;
+            for (int i = 0; i < monsterWeight.Count; i++)
+            {
+                if (monsterWeight[i] > monsterWeight[index])
+                    index = i;
+            }
+            instance++;
+            return index;
+        }
+        
+
     }
 }
+
+
