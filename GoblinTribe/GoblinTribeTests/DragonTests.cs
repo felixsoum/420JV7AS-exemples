b36a9ec01c744751b2f9d4f678f403ab61380023
Unknown <felix@felixsoum.com>
2018-09-16 23:58:43 -0400

    Add GoblinTribe project

---
+++ GoblinTribe/GoblinTribeTests/DragonTests.cs
@@ -0,0 +1,31 @@
+﻿using GoblinTribe;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace GoblinTribeTests
+{
+    [TestClass]
+    public class DragonTests
+    {
+        [TestInitialize]
+        public void Init()
+        {
+            Goblin.Reset();
+        }
+
+        [TestMethod]
+        public void TestHp()
+        {
+            Assert.IsTrue(new Dragon().GetHp() > new Goblin().GetHp());
+        }
+
+        [TestMethod]
+        public void TestFireTribe()
+        {
+            Assert.AreNotEqual(Goblin.Tribe, "Fire");
+
+            new Dragon();
+
+            Assert.AreEqual(Goblin.Tribe, "Fire");
+        }
+    }
+}
